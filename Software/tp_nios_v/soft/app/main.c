7d1702728b3ef8e2e6e89d868b8582d10394146f
Mantoulaye <mbenguemantou@gmail.com>
2026-01-08 09:46:55 +0100

    Chenillard

--- Software/tp_nios_v/soft/app/main.c
+++ Software/tp_nios_v/soft/app/main.c
@@ -9,3 +9,25 @@ int main (void)
 
 	return 0;
 }
+
+int chenillard(int delay_us)
+{
+	int nombre_leds=10;
+	printf("Lancement du chenillard...\n");
+	while (1)
+	{
+		// Turn on each LED in sequence
+		for (int i = 0; i < nombre_leds; i++)
+		{
+			IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE, 1 << i);
+			usleep(delay_us);
+		}
+		// Turn off each LED in reverse sequence
+		for (int i = 6; i >= 0; i--)
+		{
+			IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE, 1 << i);
+			usleep(delay_us);
+		}
+	}
+	return 0;
+}
